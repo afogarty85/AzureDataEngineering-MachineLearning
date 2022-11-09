c742d863ff27f3dc012e1d8a9bc0a592df373ccb
afogarty85 <apf2bb@virginia.edu>
2022-11-09 08:03:08 -0500

    strip args ref

--- Python/datalake_download_files.py
+++ Python/datalake_download_files.py
@@ -29,7 +29,7 @@ async def concat_parquet_adls_files(self, file_path, columns):
     '''
     # create credentials
     # get access to AAD
-    async with ClientSecretCredential(self.args.TENANT, self.args.CLIENTID, self.args.CLIENTSECRET) as credential:
+    async with ClientSecretCredential(tenant, client_id, client_secret) as credential:
         # get access to ADLS
         async with DataLakeServiceClient(account_url=f"https://lakelocation.dfs.core.windows.net", credential=credential) as datalake_service_client:
             # get access to FS
@@ -43,12 +43,12 @@ async def concat_parquet_adls_files(self, file_path, columns):
                 for path in path_storage:
                     file_client = file_system_client.get_file_client(f'{path}')
                     # download file, concurrency is parallel connections
-                    self.logger.info(f'Downloading {path}')
+                    logger.info(f'Downloading {path}')
                     download = await file_client.download_file(max_concurrency=75)
                     downloaded_bytes = await download.readall()
                     byte_storage.append(downloaded_bytes)
             # concat data
-            self.logger.info('Concatenating parquet files')
+            logger.info('Concatenating parquet files')
             full_df = pd.concat(pd.read_parquet(BytesIO(parquet_file), columns=columns)
                 for parquet_file in byte_storage)
 
