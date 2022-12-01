61e1282c14876bc159a180c1f3468ba04c0030de
afogarty85 <apf2bb@virginia.edu>
2022-11-30 21:51:22 -0500

    improve batch ado generator / processor

--- Python/devops_batch_download_all.py
+++ Python/devops_batch_download_all.py
@@ -30,6 +30,7 @@ class ado_generator_by_batch():
         self.headers = config.headers
         self.url = config.url
         self.logger = logger
+        self.args = args
 
     async def async_send_request(self, url, req, headers, type, session, semaphore):
         """
@@ -41,13 +42,13 @@ class ado_generator_by_batch():
                 if semaphore.locked():
                     await asyncio.sleep(1)
                 async with session.get(url=req, headers=self.headers) as resp:
-                    return await resp.json()
+                    return await resp.read()
 
         if type == 'post':
             async with semaphore:
                 if semaphore.locked():
                     await asyncio.sleep(1)
-                async with session.post(url=self.url + "_apis/wit/wiql?api-version=6.0", headers=self.headers, json={"query": req}) as resp:
+                async with session.post(url=self.url + "_apis/wit/wiql?timePrecision=true&api-version=7.0", headers=self.headers, json={"query": req}) as resp:
                     return await resp.json()
 
     @retry(wait=wait_exponential(multiplier=2, min=15, max=45), stop=stop_after_attempt(10))
@@ -67,14 +68,28 @@ class ado_generator_by_batch():
         (4): join (2) and (3)
         (5): clean up
         '''
+        def date_imputer(days):
+            ''' gen dates for work id searching '''
+            the_date = (pd.Timestamp("today", tz="US/Eastern") - datetime.timedelta(days=days))
+            return the_date.strftime('%Y-%m-%d')
 
-        # time travel to 2017 feb
-        url_post_list = [f"Select [System.Id] From WorkItems where [System.TeamProject] = @project AND [System.CreatedDate] <= @today-{i} AND [System.CreatedDate] >= @today-{21+i}" for i in range(0, 1900, 22)]
+        if self.args.lastrev:
+            self.logger.info('Getting work items that have been modified in the last 3 days...')
+            # last 3 days
+            url_post_list = [f"Select [System.Id] From WorkItems where [System.TeamProject] = @project AND [System.ChangedDate] <= @today-{i} AND [System.ChangedDate] >= @today-{3}" for i in range(0, 11)]
+        else:
+            self.logger.info('Grabbing entire history of work items...')
+            # since jan 1 2019; fairly obtuse way to get lots of workids owing to devops arbitray limitations
+            url_post_list1 = [f"Select [System.Id] From WorkItems where [System.TeamProject] = @project AND [System.CreatedDate] >= '{date_imputer(i)}T00:00:00.0000000' AND [System.CreatedDate] <= '{date_imputer(0+i)}T08:00:00.0000000'" for i in range(0, 1429, 1)]
+            url_post_list2 = [f"Select [System.Id] From WorkItems where [System.TeamProject] = @project AND [System.CreatedDate] >= '{date_imputer(i)}T08:00:00.0000000' AND [System.CreatedDate] <= '{date_imputer(0+i)}T14:59:59.9999999'" for i in range(0, 1429, 1)]
+            url_post_list3 = [f"Select [System.Id] From WorkItems where [System.TeamProject] = @project AND [System.CreatedDate] >= '{date_imputer(i)}T14:59:59.9999999' AND [System.CreatedDate] <= '{date_imputer(0+i)}T23:59:59.9999999'" for i in range(0, 1429, 1)]
+            url_post_list = url_post_list1 + url_post_list2 + url_post_list3
 
         # get work ids
         loop = asyncio.get_event_loop()
-        out = loop.run_until_complete(self.async_main_request(url=self.url, semaphore=1000, req_list=url_post_list, headers=self.headers, type='post'))
+        out = loop.run_until_complete(self.async_main_request(url=self.url, semaphore=64, req_list=url_post_list, headers=self.headers, type='post'))
         workitem_set = pd.concat([pd.json_normalize(_['workItems']) for _ in out]).reset_index(drop=True)['id'].values
+        self.logger.info(f'Found this many work items: {workitem_set.shape[0]}')
 
         # get work item history
         url_list = [self.url + f'_apis/wit/workitems/{id}/revisions?$expand=all&$skip=0&api-version=6.0' for id in workitem_set]
@@ -92,31 +107,35 @@ class ado_generator_by_batch():
         # combine batch_set
         batch_set = list(sum(batch_set, []))
 
+        # filter
+        batch_set = [i for i in batch_set if len(i) != 0 and i[:8].decode('utf-8') == '{"count"']
+
         self.logger.info('Generating work ids and those that need revs...')
         # get # of revs from each work id
-        total_revs = pd.concat([pd.json_normalize(_)['count'] for _ in batch_set])
+        total_revs = pd.concat([pd.json_normalize(json.loads(_)) for _ in batch_set])['count']
         retry_list = [200 if i == 200 else 0 for i in total_revs.values]
 
         self.logger.info('Unpacking work items...')
         # unpack the work ids
-        workItems = pd.concat([pd.json_normalize(_['value']) for _ in batch_set])
+        workItems = pd.concat([pd.json_normalize(json.loads(_)['value']) for _ in batch_set])
 
         # send new requests for those that broke 200 revs
         indices_of_interest = [i for i, j in enumerate(retry_list) if j != 0]
         ids_of_interest = [j for i, j in enumerate(workitem_set) if i in indices_of_interest]
 
         # rebuild new urls
-        self.logger.info(f'Sending these extra revs {len(ids_of_interest)}...')
-        url_retry_list = [self.url + f'_apis/wit/workitems/{id}/revisions?$expand=all&$skip=200&api-version=6.0' for id in ids_of_interest]
-        loop = asyncio.get_event_loop()
-        out = loop.run_until_complete(self.async_main_request(url=self.url, semaphore=64, req_list=url_retry_list, headers=self.headers, type='get'))
+        if len(ids_of_interest) > 0:
+            self.logger.info(f'Sending these extra revs {len(ids_of_interest)}...')
+            url_retry_list = [self.url + f'_apis/wit/workitems/{id}/revisions?$expand=all&$skip=200&api-version=6.0' for id in ids_of_interest]
+            loop = asyncio.get_event_loop()
+            out = loop.run_until_complete(self.async_main_request(url=self.url, semaphore=64, req_list=url_retry_list, headers=self.headers, type='get'))
 
-        # get extended items
-        workItems_extended = pd.concat([pd.json_normalize(_['value']) for _ in out])
+            # get extended items
+            workItems_extended = pd.concat([pd.json_normalize(json.loads(_)['value']) for _ in out])
 
-        # concat final results
-        self.logger.info('Concatenating final results...')
-        workItems = pd.concat([workItems_extended, workItems], axis=0)
+            # concat final results
+            self.logger.info('Concatenating final results...')
+            workItems = pd.concat([workItems_extended, workItems], axis=0)
 
         # drop duplicates
         self.logger.info('Cleaning up the dataframe...')
@@ -129,14 +148,14 @@ class ado_generator_by_batch():
         if self.config.cols is not None:
             workItems = workItems[self.config.cols]
 
-        # drop cols
+        # drop cols identified in config
         try:
             workItems = workItems.drop(self.config.drop, axis=1)
         except Exception as e:
             self.logger.error(f" {e}")
 
         # find unnecessary cols
-        drop_list = workItems.filter(regex='.descriptor|.id|.imageUrl|.href').columns.tolist()
+        drop_list = workItems.filter(regex='.descriptor|.id|.imageUrl|.href|.url').columns.tolist()
 
         # drop them
         workItems = workItems.drop(drop_list, axis=1)
@@ -148,5 +167,10 @@ class ado_generator_by_batch():
         for col in s_cols:
             if workItems[col].astype(str).str.contains("<span").any():
                 workItems[col] = workItems[col].str.replace('<[^<]+?>', ' ', regex=True)
+
+        # reset index again
+        workItems = workItems.reset_index(drop=True)
+
+        self.logger.info(f'Saving a dataframe of this shape: {workItems.shape}')
 
         return workItems
