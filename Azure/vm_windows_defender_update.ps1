d1be883474ee567a9e81adb010d4c664c7b53dd7
afogarty85 <apf2bb@virginia.edu>
2023-04-03 14:23:33 -0400

    add windows defender update ps for azure vm

---
+++ Azure/vm_windows_defender_update.ps1
@@ -0,0 +1,10 @@
+$vdmpathbase = "$env:systemdrive\wdav-update\{00000000-0000-0000-0000-"
+$vdmpathtime = Get-Date -format "yMMddHHmmss"
+$vdmpath = $vdmpathbase + $vdmpathtime + '}'
+$vdmpackage = $vdmpath + '\mpam-fe.exe'
+
+New-Item -ItemType Directory -Force -Path $vdmpath | Out-Null
+
+Invoke-WebRequest -Uri 'https://go.microsoft.com/fwlink/?LinkID=121721&arch=x64' -OutFile $vdmpackage
+
+Start-Process -FilePath $vdmpackage -WorkingDirectory $vdmpath -ArgumentList "/x"
