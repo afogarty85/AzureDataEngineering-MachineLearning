6fd3f6420a7266e026d4af505985b76ecc803ee3
afogarty85 <apf2bb@virginia.edu>
2023-06-28 20:19:18 -0400

    update vm av

--- Azure/vm_windows_defender_update.ps1
+++ Azure/vm_windows_defender_update.ps1
@@ -1,9 +1,15 @@
 # keep VM up-to-date with anti-virus
 
-wget https://go.microsoft.com/fwlink/?LinkId=197094 -OutFile nis.exe
+wget https://go.microsoft.com/fwlink/?LinkId=197094 -OutFile C:\nis.exe;
 
-wget https://go.microsoft.com/fwlink/?LinkId=121721"&"arch=x64 -OutFile mpam-fe.exe
+wget https://go.microsoft.com/fwlink/?LinkId=121721"&"arch=x64 -OutFile C:\mpam-fe.exe;
 
-nis.exe
+wget https://go.microsoft.com/fwlink/?LinkID=87341 -OutFile C:\mpfam-fe64.exe;
 
-mpam-fe.exe
+.C:\nis.exe;
+
+.C:\mpam-fe.exe;
+
+.C:\mpfam-fe64.exe;
+
+Get-MpComputerStatus | select *updated, *version
