fd310b584a1cdda30f11d00c8e77be7708303f94
afogarty85 <apf2bb@virginia.edu>
2023-06-30 21:39:01 -0400

    add some basic ray functionality

---
+++ MachineLearning/Ray/azureml_ray_basic_functions.py
@@ -0,0 +1,72 @@
+
+from adlfs import AzureBlobFileSystem
+from azure.keyvault.secrets import SecretClient
+from azure.identity import DefaultAzureCredential
+import pyarrow.compute as pc
+import ray
+import psutil
+import pandas as pd
+from ray.data import ActorPoolStrategy
+
+
+if not ray.is_initialized():
+    ray.init()
+
+# repartition for parallel
+num_cpus = psutil.cpu_count(logical=False)
+num_partitions = min(num_cpus - 2, 32)
+
+# to use managed identity, requires azure-keyvault-secrets==4.6.0 azure-identity==1.11.0; useful for jupyter
+secret_client = SecretClient(vault_url="https://moaddev6131880268.vault.azure.net/", credential=DefaultAzureCredential())
+
+# set storage options
+storage_options={
+                 'account_name': 'moaddevlake',
+                 'tenant_id': "72f988bf-86f1-41af-91ab-2d7cd011db47",
+                 'client_id': secret_client.get_secret("dev-synapse-sqlpool-sp-id").value,
+                 'client_secret': secret_client.get_secret("dev-synapse-sqlpool-sp-secret").value,
+                 }
+
+# apply storage connector info
+abfs = AzureBlobFileSystem(**storage_options)
+
+# get the data and filter
+ds = ray.data.read_parquet(
+    ["az://chiemoaddevfs/out/Gold/ML/RandomizedTest/"],  # container / path
+    filesystem=abfs,
+    use_threads=True,
+    filter=pc.field('instance_id').isin(['20230613']),   
+    parallelism=num_partitions
+).repartition(num_blocks=num_partitions, shuffle=False)
+
+# get count
+ds.count()
+
+# get columns
+ds.columns()
+
+# udf map
+def transform_df(df: pd.DataFrame) -> pd.DataFrame:
+
+    # recode lower counts
+    threshold = 75000
+
+    # identify rows with less than threshold occurrences
+    m = df.groupby('SYNDROME')['SYNDROME'].transform('size').lt(threshold)
+
+    # update
+    df.loc[m, 'SYNDROME'] = 'other'
+
+    # factorize label
+    df["SYNDROME"] = pd.factorize(df['SYNDROME'])[0]
+    return df
+
+# transform
+ds = ds.map_batches(
+                    transform_df,
+                    compute=ActorPoolStrategy(min_size=2, max_size=num_cpus), # how many CPU cores to claim
+                    batch_format="pandas",
+                    )
+
+# trigger operation
+ds.count()
